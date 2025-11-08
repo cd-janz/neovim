c48a23b60c49b6f63fe08da4a6acdc897d0dafeb
cd-janz <cd.juanzuniga@gmail.com>
2025-11-08 01:42:18 -0500

    dashboard header path updated

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,43 +1,42 @@
 return {
-  "nvimdev/dashboard-nvim",
-  event = "VimEnter",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons"
-  },
-  config = function()
-    local header = vim.fn.readfile(vim.fn.expand("~/dotfiles/.assets/ansinascii/nvim.txt"))
-    require("dashboard").setup({
-      theme = "hyper",
-      shortcut_type = true,
-      config = {
-        header = header,
-        packages = { enable = true },
-        week_header = {
-          enable = true
-        },
-        shortcut = {
-          { desc = " dotfiles ", group = "Label", action = "OpenDotfilesConfig", key = "d" },
-        },
-        project = {
-          enable = true,
-          limit = 5,
-          icon = '  ',
-          label = 'projectos recientes',
-          action = "Telescope find_files cwd="
-        },
-        mru = {
-          enable = true,
-          limit = 5,
-          icon = '  ',
-          label = "Archivos recientes",
-          cwd_only = true
-        },
-        footer = {
-        },
-      },
-      hide = {
-        lualine = true
-      },
-    })
-  end
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local header = vim.fn.readfile(vim.fn.expand("~/dotfiles/assets/ansinascii/nvim.txt"))
+		require("dashboard").setup({
+			theme = "hyper",
+			shortcut_type = true,
+			config = {
+				header = header,
+				packages = { enable = true },
+				week_header = {
+					enable = true,
+				},
+				shortcut = {
+					{ desc = " dotfiles ", group = "Label", action = "OpenDotfilesConfig", key = "d" },
+				},
+				project = {
+					enable = true,
+					limit = 5,
+					icon = "  ",
+					label = "projectos recientes",
+					action = "Telescope find_files cwd=",
+				},
+				mru = {
+					enable = true,
+					limit = 5,
+					icon = "  ",
+					label = "Archivos recientes",
+					cwd_only = true,
+				},
+				footer = {},
+			},
+			hide = {
+				lualine = true,
+			},
+		})
+	end,
 }
